03236815dd75688221608e4c4ef4de7f6cfb4e06
Matt Kane <matt@gatsbyjs.com>
2021-03-27 12:47:56 +0000

    SEO

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet";
 import { layout, visuallyHidden } from "./layout.module.css";
 import "../style/style.css";
 import ogimage from "../images/departure.webp";
+import { graphql, useStaticQuery } from "gatsby";
 interface Props {
   title?: string;
 }
@@ -10,9 +11,21 @@ export const Layout: React.FC<Props> = function Layout({
   title = "Station Arrival and Departure Boards",
   children,
 }) {
+  const { site } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          siteUrl
+        }
+      }
+    }
+  `);
+  const url = site?.siteMetadata?.siteUrl;
   return (
     <div className={layout}>
       <Helmet title={title} htmlAttributes={{ lang: "en" }}>
+        <meta property="twitter:title" content={title} />
+        <meta property="og:title" content={title} />
         <meta
           name="description"
           content="Live station arrival and departure boards"
@@ -22,8 +35,8 @@ export const Layout: React.FC<Props> = function Layout({
           content="Live station arrival and departure boards"
         />
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="og:image" content={ogimage} />
-        <meta property="twitter:image" content={ogimage} />
+        <meta property="og:image" content={`${url}${ogimage}`} />
+        <meta property="twitter:image" content={`${url}${ogimage}`} />
         <meta property="twitter:creator" content="@ascorbic" />
       </Helmet>
       <h1 className={visuallyHidden}>{title}</h1>
